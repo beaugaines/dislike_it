11d59e7f7cd14b860bc7f27773f926bd61fd2e0a
Charlie Gaines <beaugaines@gmail.com>
2014-01-12 23:20:50 -0500

    vergin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DislikeIt::Application.config.secret_key_base = '052cc104beb4aec1f9b3a6b507c41ed640bc389091a14e138cc74975158b420f24a6a823b3f4fbe0e05fd8060210a7ea59840e207cf2c018c9a0dd40dad3030b'
